9455b7a18906f6c249a2662e873be2eea6e6d4a1
bagwanje003 <165358717+bagwanje003@users.noreply.github.com>
2024-03-29 23:03:05 +0400

    create lessons for frontend

---
+++ lessons.js
@@ -0,0 +1,41 @@
+const LESSONS = [
+    {
+      id: 1,
+      subject: 'Music',
+      location: 'Germany',
+      price: 20,
+      spaces: 5,
+      image: "images/Music.jpeg"
+        },
+    {
+      id: 2,
+      subject: 'Science',
+      location: 'United States',
+      price: 25,
+      spaces: 3,
+      image: "images/Science.jpeg"
+        },
+    {
+      id: 3,
+      subject: 'Mathematics',
+      location: 'France',
+      price: 15,
+      spaces: 8,
+      image: "images/Mathematics.jpeg"
+        },
+    {
+      id: 4,
+      subject: 'English',
+      location: 'United Kingdom',
+      price: 18,
+      spaces: 6,
+      image: "images/English.jpeg"
+        },
+    {
+      id: 5,
+      subject: 'Programming',
+      location: 'Canada',
+      price: 22,
+      spaces: 4,
+      image: "images/Programming.jpeg"
+        },
